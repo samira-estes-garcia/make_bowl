ed3818f32ffa2580e352c4f46a14d6eb9856e6e2
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-09-16 19:55:25 -0500

    added prelim fetch for ingredients, basic html and css

---
+++ make_bowl-frontend/src/index.js
@@ -0,0 +1,32 @@
+const BASE_URL = "http://localhost:3000";
+const INGREDIENTS_URL = `${BASE_URL}/ingredients`;
+const BOWLS_URL = `${BASE_URL}/bowls`;
+
+class Ingredients {}
+
+const getIngredients = () => {
+  return fetch(INGREDIENTS_URL).then((res) => res.json());
+};
+
+let ingredientsContainer = document.querySelector("#container");
+
+getIngredients().then((json) => {
+  json.forEach((ingredient) => {
+    let ingredientElement = document.createElement("div");
+    ingredientElement.setAttribute("class", "card");
+    ingredientElement.dataset.id = ingredient.id;
+    ingredientElement.innerHTML = renderElement(ingredient);
+    ingredientsContainer.append(ingredientElement);
+  });
+});
+
+function renderElement(ingredient) {
+  return `  <div class="ingredients-info">
+            <p>${ingredient.name}</p>
+            <p>${ingredient.price}</p>
+            <p>${ingredient.category}</p>
+            </div>
+            <div class="button-container">
+            <button class="add"><img src="assets/add.png" class="add-img" /></button>
+            </div>`;
+}
